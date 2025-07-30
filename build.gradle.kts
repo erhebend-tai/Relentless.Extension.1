e51b327a45a8935eb7c77ab0a096a865d1d629e8
erhebend-tai <erhebend@protonmail.com>
2025-07-30 05:41:19 -0500

    2541

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,10 @@ repositories {
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
+    implementation(libs.firebase.admin)
+    // implementation(libs.mistralai.client)
+    // implementation(libs.cloudflare.ai)
+    implementation(libs.mongodb.driver)
     testImplementation(libs.junit)
     testImplementation(libs.opentest4j)
 
